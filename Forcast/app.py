6492bcef9e0ad06b67bbb1b729202424c5e963d9
wasifch2 <96172617+wasifch2@users.noreply.github.com>
2025-06-02 18:18:44 +0200

    Update app.py

--- Forcast/app.py
+++ Forcast/app.py
@@ -7,7 +7,6 @@ Original file is located at
     https://colab.research.google.com/drive/1ssG3H7e00Lbd1rwJxNoD80nGEZBll2Ue
 """
 
-!pip install streamlit
 
 import streamlit as st
 import pandas as pd
